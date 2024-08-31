fc85dfd010f63d595e4402d6f556381eb8e3255a
b4shm3rlow <ludovicoguercio88@gmail.com>
2024-08-31 18:31:41 +0200

    refactor app to webapp, add the routes and the service for face model

--- app.py
+++ app.py
@@ -1,125 +1,50 @@
-import cv2
-import pyaudio
-import wave
-import threading
-import tkinter as tk
-from tkinter import messagebox
-from services.face_emotion_service import FaceEmotionPredictor
+from flask import Flask, render_template, Response
+from services.face_emotion_service import FaceEmotionService
 from services.speech_emotion_service import speechEmotionPredictor
 
-class EmotionRecognitionApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Multimodal Emotion Recognition App")
-        
-        self.face_predictor_path = FaceEmotionPredictor('path/to/face_emotion_model.h5')
-        self.speech_predictor_path = speechEmotionPredictor('path/to/speech_emotion_model.h5')
+app = Flask(__name__)
 
-        self.is_running = False
-        self.audio_frames = []
-        self.cap = None
-        self.audio_thread = None
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        self.menu_app()
+# rotta per il rendering della pagina html per il riconiscimento emozioni facciale, qui é collegato l url per la rotta per lo streaming video e l esecuzione del modello
+@app.route('/face_emotion')
+def face_emotion():
+    return render_template('face_service.html')
 
-    def menu_app(self):
-        # Clear the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
+# funzione che genera uno stream di immagini video da una camera
+def gen(camera):
+    while True:
+        frame = camera.predict()
 
-        # Create Start buttons
-        start_face_button = tk.Button(self.root, text="Face Emotion Recognition", command=self.start_face_recognition)
-        start_speech_button = tk.Button(self.root, text="Speech Emotion Recognition", command=self.start_speech_recognition)
-        exit_app_button = tk.Button(self.root, text="Exit", command=self.root.quit)
-        
-        start_face_button.pack(pady=20)
-        start_speech_button.pack(pady=20)
-        exit_app_button.pack(pady=20)
-    
-    def exit_service(self, stop_command):
-        for widget in self.root.winfo_children():
-            widget.destroy()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-        stop_button = tk.Button(self.root, text="Quit", command=stop_command)
-        stop_button.pack(pady=20)
-    
-    def exit_app(self):
-        self.is_running=True
+# rotta in cui viene eseguito lo streaming e il modello
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(FaceEmotionService()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def start_face_recognition(self):
-        self.is_running = True
-        self.cap = cv2.VideoCapture(0)
-        self.exit_service(self.stop_face_recognition)
-        self.face_recognition_loop()
+'''@app.route('/face_emotion/predict', methods=['POST'])
+def predict_face_emotion():
+    # Here, you'll receive image data from the client side
+    image_data = request.files['image'].read()
+    emotion = face_predictor.predict(image_data)
+    return jsonify({'emotion': emotion})
 
-    def stop_face_recognition(self):
-        self.is_running = False
-        if self.cap:
-            self.cap.release()
-        cv2.destroyAllWindows()
-        self.menu_app()
+@app.route('/speech_emotion', methods=['GET'])
+def speech_emotion():
+    #TODO
+    speech_predictor = speechEmotionPredictor('path/to/speech_emotion_model.h5')
+    return
 
-    def face_recognition_loop(self):
-        while self.is_running:
-            ret, frame = self.cap.read()
-            if not ret:
-                print("Errore: Impossibile catturare frame dalla webcam.")
-                break
-            
-            frame_with_emotion = self.face_predictor_path.predict(frame) #prediction
-            cv2.imshow('Face Emotion Recognition', frame_with_emotion) #show frame with label
+@app.route('/speech_emotion/predict', methods=['POST'])
+def predict_speech_emotion():
+    # Here, you'll receive audio data from the client side
+    audio_data = request.files['audio'].read()
+    emotion = speech_predictor.predict(audio_data)
+    return jsonify({'emotion': emotion})'''
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                self.stop_face_recognition()
-            else:
-                self.root.after(10, self.face_recognition_loop) #continue every 10 millsec
-
-    def start_speech_recognition(self):
-        self.is_running = True
-        self.audio_frames = []
-        self.audio_thread = threading.Thread(target=self.capture_audio)
-        self.audio_thread.start()
-        self.show_stop_record_button(self.check_speech_recognition) #stop recording
-        self.exit_service(self.)
-
-    def check_speech_recognition(self):
-        self.is_running = False
-        if self.audio_thread:
-            self.audio_thread.join()
-        self.save_audio()
-        speech_emotion = self.speech_predictor_path.predict("output.wav")
-        print(f"Detected: {speech_emotion}")
-        messagebox.showinfo(f"Detected: {speech_emotion}")
-        #back to menu
-        self.menu_app()
-
-    def capture_audio(self):
-        CHUNK = 1024
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1
-        RATE = 44100
-        
-        p = pyaudio.PyAudio()
-        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-
-        while self.is_running:
-            data = stream.read(CHUNK)
-            self.audio_frames.append(data)
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-    def save_audio(self):
-        wf = wave.open("output.wav", 'wb')
-        wf.setnchannels(1)
-        wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paInt16))
-        wf.setframerate(44100)
-        wf.writeframes(b''.join(self.audio_frames))
-        wf.close()
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = EmotionRecognitionApp(root)
-    root.mainloop()
+if __name__ == '__main__':
+    app.run(debug=True)
--- services/face_emotion_service.py
+++ services/face_emotion_service.py
@@ -1,79 +1,48 @@
 import cv2
 import numpy as np
 from keras.api.models import load_model
-#from utils.emotions import EMOTIONS
+from keras.api.preprocessing.image import img_to_array
 import os
 import tensorflow as tf
 
-# app base funziona correttamente, implemetare una migliore app; non funziona se lancio da fedora (problemi con Cuda e tensorflow che non riconosce i driver da fedora pdio)
-#TODO: migliorare la grafica dell applicazione; provare a migliorare il modello (prima priorita a fare quello della voce)
-
-
-EMOTIONS = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+LABELS = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-#os.environ['CUDA_VISIBLE_DEVICES'] = ''
-#tf.get_logger().setLevel('ERROR')
 
-class FaceEmotionPredictor:
-    def __init__(self, model_path):
-        self.model = load_model(model_path) # Loads a model saved via model.save()
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        self.emotions = EMOTIONS
+class FaceEmotionService:
+    def __init__(self):
+        self.model = load_model('models/face/mini_xception2_checkpoint.model.keras')
+        self.face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.cap = cv2.VideoCapture(0)
+        self.labels = LABELS
+    
+    def __del__(self):
+        self.cap.release()
 
-    def predict(self, frame):
+    def predict(self):
+        ret, frame = self.cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        faces = self.face_detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
         for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
             roi_gray = gray[y:y + h, x:x + w]
             roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            roi_gray = roi_gray.astype('float') / 255.0
-            roi_gray = np.expand_dims(roi_gray, axis=0)
-            roi_gray = np.expand_dims(roi_gray, axis=-1)
+            
+            if np.sum([roi_gray]) != 0:
+                roi_gray = roi_gray.astype('float') / 255.0
+                roi_gray = img_to_array(roi_gray)
+                roi_gray = np.expand_dims(roi_gray, axis=0)
+                #roi_gray = np.expand_dims(roi_gray, axis=-1)
 
-            prediction = self.model.predict(roi_gray)[0]
-            emotion = self.emotions[np.argmax(prediction)]
+                prediction = self.model.predict(roi_gray)[0]
+                emotion = self.labels[np.argmax(prediction)]
+                print(f'Face emotion detected: {emotion}')
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-        return frame
-
-# # # # # # # # #
-#    Testing    #
-# # # # # # # # #
-
-def test_face_emotion_predictor_realtime(model_path):
-    predictor = FaceEmotionPredictor(model_path)
-
-    # Inizializza la webcam
-    cap = cv2.VideoCapture(0)
-
-    if not cap.isOpened():
-        print("Errore: Impossibile aprire la webcam.")
-        return
-
-    while True:
-        # Cattura frame-by-frame
-        ret, frame = cap.read()
-
-        if not ret:
-            print("Errore: Impossibile catturare frame dalla webcam.")
-            break
-
-        # Esegui la previsione delle emozioni
-        frame_with_predictions = predictor.predict(frame)
-
-        # Mostra il frame con le previsioni delle emozioni
-        cv2.imshow('Face Emotion Predictor', frame_with_predictions)
-
-        # Esci dal ciclo premendo 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Rilascia la webcam e chiudi le finestre
-    cap.release()
-    cv2.destroyAllWindows()
-
-face_model_path = 'models/face/mini_xception2_checkpoint.model.keras'
-test_face_emotion_predictor_realtime(face_model_path)
+                cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            else:
+                cv2.putText(frame, 'No Emotion Detected', (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0, 255, 0), 2)
+        
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        
+        return (jpeg.tobytes())
