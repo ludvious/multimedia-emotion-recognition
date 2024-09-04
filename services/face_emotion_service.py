681f2f1cec0b6d22da04fcbdc4cbd89641db6964
b4shm3rlow <ludovicoguercio88@gmail.com>
2024-09-04 16:15:46 +0200

    fix

--- services/face_emotion_service.py
+++ services/face_emotion_service.py
@@ -12,7 +12,7 @@ class FaceEmotionService:
     def __init__(self):
         self.model = load_model('models/face/mini_xception2_checkpoint.model.keras')
         self.face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0) #index relative the webcam (0 if you have only preset cam installed, index can be different if you have more cam plugged on your OS)
         self.labels = LABELS
     
     def __del__(self):
