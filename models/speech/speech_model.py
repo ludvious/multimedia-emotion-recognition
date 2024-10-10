9f165fd48ce5d8556f3a2cdb8def13325f79b62a
b4shm3rlow <ludovicoguercio88@gmail.com>
2024-10-10 21:23:14 +0200

    fix

--- models/speech/speech_model.py
+++ models/speech/speech_model.py
@@ -7,10 +7,11 @@ from preprocessing.dataset import create_audio_dataset, process_audio_data_gener
 from keras.api.utils import to_categorical
 
 class AudioModel:
-    def __init__(self, num_labels: int, input_shape, batch_size: int) -> None:
+    def __init__(self, num_labels: int, input_shape, batch_size: int, epochs: int) -> None:
         self.num_labels = num_labels
         self.input_shape = input_shape
         self.batch_size = batch_size
+        self.epochs = epochs
         self.model = self._create_model()
 
     def _create_model(self):
@@ -47,19 +48,19 @@ class AudioModel:
 
         return train, validation
 
-    def train_model(self, train, validation, epochs=10, verbose = 1):
+    def train_model(self, train, validation, verbose = 1):
         
         #X_train, X_val, y_train, y_val = train_test_split(features, labels, test_size=0.2, random_state=42)
         # add callbacks to review for this model
         early_stop = EarlyStopping('val_loss', patience=10)
         reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=5, verbose=1) # Reduce learning rate when a metric has stopped improving
-        checkpoint_models_path = 'models/face/mini_xception_'+'checkpoint.model.keras'
+        checkpoint_models_path = 'models/speech/audio_model'+'checkpoint.model.keras'
         model_checkpoint = ModelCheckpoint(filepath=checkpoint_models_path, monitor='val_loss', verbose=verbose, save_best_only=True)
         callbacks = [model_checkpoint, early_stop, reduce_lr]
         print(f"add callbacks ...\n")
 
         print(f"Start training ... \n")
-        history = self.model.fit(train, batch_size=self.batch_size, epochs=epochs, validation_data=validation, callbacks=callbacks)
+        history = self.model.fit(train, batch_size=self.batch_size, epochs=self.epochs, validation_data=validation, callbacks=callbacks)
 
         return history
 
