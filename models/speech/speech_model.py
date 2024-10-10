c859b338ffdf4f1b631112e27f2a22f291133cfb
b4shm3rlow <ludovicoguercio88@gmail.com>
2024-10-10 19:14:35 +0200

    fix model audio

--- models/speech/speech_model.py
+++ models/speech/speech_model.py
@@ -3,7 +3,7 @@ from keras.api.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from keras.api.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from preprocessing.dataset import create_audio_dataset
+from preprocessing.dataset import create_audio_dataset, process_audio_data_generator
 from keras.api.utils import to_categorical
 
 class AudioModel:
@@ -13,22 +13,23 @@ class AudioModel:
         self.batch_size = batch_size
         self.model = self._create_model()
 
-    def _create_model(self, X_train):
+    def _create_model(self):
 
-        input_shape = X_train[0].shape
-        input_layer = Input(shape=input_shape)
-        x = Conv2D(16, (3, 3), activation='relu')(input_layer)
-        x = MaxPooling2D((2, 2))(x)
-        x = Dropout(0.2)(x)
-        x = Conv2D(32, (3, 3), activation='relu')(input_layer)
+        img_input = Input(self.input_shape)
+        x = Conv2D(32, (3, 3), activation='relu')(img_input)
         x = MaxPooling2D((2, 2))(x)
         x = Dropout(0.2)(x)
         x = Conv2D(64, (3, 3), activation='relu')(x)
         x = MaxPooling2D((2, 2))(x)
+        x = Dropout(0.2)(x)
+        x = Conv2D(64, (3, 3), activation='relu')(x)
         x = Flatten()(x)
         x = Dense(64, activation='relu')(x)
-        output_layer = Dense(self.num_labels, activation='softmax')(x)
-        model = Model(input_layer, output_layer)
+        x = Dropout(0.2)(x)
+        x = Dense(32, activation='relu')(x)
+        # Output layer
+        output = Dense(24, activation='softmax')(x)
+        model = Model(img_input, output)
 
         print(f"Creating Model ...\n")
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -40,14 +41,15 @@ class AudioModel:
     def create_dataset(self, path_data: str):
         print(f"Loading and preprocess the dataset ...\n")
         # Split data into training and testing sets
-        features, labels = create_audio_dataset(path_data, self.num_labels)
-        labels = to_categorical(labels, num_labels=self.num_labels)  # Convert labels to one-hot encoding
+        #features, labels = create_audio_dataset(path_data, self.num_labels)
+        train, validation = process_audio_data_generator(path_data, self.batch_size, self.target_shape)
+        #labels = to_categorical(labels, num_labels=self.num_labels)  # Convert labels to one-hot encoding
 
-        return features, labels
+        return train, validation
 
-    def train_model(self, features, labels, epochs=100, verbose = 1):
+    def train_model(self, train, validation, epochs=10, verbose = 1):
         
-        X_train, X_val, y_train, y_val = train_test_split(features, labels, test_size=0.2, random_state=42)
+        #X_train, X_val, y_train, y_val = train_test_split(features, labels, test_size=0.2, random_state=42)
         # add callbacks to review for this model
         early_stop = EarlyStopping('val_loss', patience=10)
         reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=5, verbose=1) # Reduce learning rate when a metric has stopped improving
@@ -57,7 +59,7 @@ class AudioModel:
         print(f"add callbacks ...\n")
 
         print(f"Start training ... \n")
-        history = self.model.fit(X_train, y_train, batch_size=self.batch_size, epochs=epochs, validation_data=(X_val, y_val), callbacks=callbacks)
+        history = self.model.fit(train, batch_size=self.batch_size, epochs=epochs, validation_data=validation, callbacks=callbacks)
 
         return history
 
